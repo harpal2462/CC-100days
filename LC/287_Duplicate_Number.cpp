e6fdfb1433811b059dc2b18bb22c5e7afbb724e5
Harpal <hpchauhan321@gmail.com>
2024-03-25 16:18:40 +0530

    update lc

--- LC/287_Duplicate_Number.cpp
+++ LC/287_Duplicate_Number.cpp
@@ -25,3 +25,11 @@ public:
         return slow;
     }
 };
+// Better Solution :
+    // int findDuplicate(vector<int>& nums) {
+    //     for(int i=0;i<nums.size();i++){
+    //         if(nums[abs(nums[i]) -1] <0 ) return abs(nums[i]);
+    //         nums[abs(nums[i])-1]*=(-1);
+    //     }
+    //     return 0;
+    // }
