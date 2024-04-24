623d56a8390cc10ed596c6ee74522cc3181c70eb
Harpal <hpchauhan321@gmail.com>
2024-04-24 13:35:31 +0530

    ADD D36_E_POTD_Nth_Tribonacci_Number to LC

---
+++ LC/D36_E_POTD_Nth_Tribonacci_Number.cpp
@@ -0,0 +1,18 @@
+// 1137. D36_E_POTD_Nth_Tribonacci_Number
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        int T[n+1];
+        T[0]=0;
+        T[1]=1;
+        T[2]=1;
+        for(int i=3;i<=n;i++){
+            T[i]=T[i-1]+T[i-2]+T[i-3];
+        }
+        return T[n];
+    }
+};
+
+// Q:https://leetcode.com/problems/n-th-tribonacci-number/description/?envType=daily-question&envId=2024-04-24
