614ed23154e5c314e9b439c94a38ad17fa66a5aa
harpal2462 <hpchauhan321@gmail.com>
2024-05-17 21:42:19 +0530

    ADD D59

---
+++ LC/D59_POTD_Delete_Leaves_With_Given_Value.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// 1325. D59_POTD_Delete_Leaves_With_Given_Value
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(!root) return nullptr;
+        root->left= removeLeafNodes(root->left,target);
+        root->right= removeLeafNodes(root->right,target);
+        if(!root->left && !root->right && root->val==target) return nullptr;
+
+        return root;
+        
+    }
+};
+// Q:https://leetcode.com/problems/delete-leaves-with-a-given-value/?envType=daily-question&envId=2024-05-17
