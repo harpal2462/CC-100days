611f84a402202e6a5a58adde29ed7f80ff7889c8
harpal2462 <hpchauhan321@gmail.com>
2024-05-21 22:31:42 +0530

    ADD D63

---
+++ LC/D63_M_POTD_Subsets.cpp
@@ -0,0 +1,18 @@
+// 78. D63_M_POTD_Subsets
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n=nums.size();
+        for(int i=0;i<(1<<n);i++){
+            vector<int>subset;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)) subset.push_back(nums[j]);
+            }
+            ans.push_back(subset);
+        }
+        return ans;
+    }
+};
+
+// Q:https://leetcode.com/problems/subsets/description/?envType=daily-question&envId=2024-05-21
