ed24986a7c2a4d27e65bdbac3f6c15e2722b572a
harpal2462 <hpchauhan321@gmail.com>
2024-05-27 16:23:16 +0530

    ADD D69

---
+++ LC/D69_M_POTD_Validate_Binary_Search_Tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//  98. D69_M_POTD_Validate_Binary_Search_Tree
+
+class Solution {
+public:
+    bool dfs(TreeNode* root,long min,long max){
+       if(root==NULL) return true;
+       if(root->val<=min || root->val>=max) return false;
+       return dfs(root->left,min,root->val) && dfs(root->right,root->val,max);
+    }
+    bool isValidBST(TreeNode* root) {
+        return dfs(root,LONG_MIN,LONG_MAX);
+    }
+};
+
+// Q: https://leetcode.com/problems/validate-binary-search-tree/
