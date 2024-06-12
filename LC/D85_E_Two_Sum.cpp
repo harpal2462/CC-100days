5e3e97fd7221b5bd42be3b1a267ce36397e3f5ee
harpal2462 <hpchauhan321@gmail.com>
2024-06-12 19:33:04 +0530

    ADD D85

---
+++ LC/D85_E_Two_Sum.cpp
@@ -0,0 +1,17 @@
+// 1. D85_E_Two_Sum    // One Pass HASH solution
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end()) {
+                return {hash[complement], i};
+            }
+            hash[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+// Q;https://leetcode.com/problems/two-sum/
